3b4f600b48043b051a2587b1b7349815924c5846
mutheejj <johnmuthee547@gmail.com>
2025-04-13 17:07:33 +0300

    clients

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -366,7 +366,7 @@
                 My Jobs
             </a>
             
-            <a href="{{ route('client.jobs.create') }}" class="user-menu-item {{ request()->routeIs('client.jobs.create') ? 'active' : '' }}">
+            <a href="http://127.0.0.1:8000/client/create-job" class="user-menu-item {{ request()->routeIs('client.create-job') ? 'active' : '' }}">
                 <svg class="user-menu-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd"></path>
                 </svg>
@@ -377,7 +377,7 @@
                 <svg class="user-menu-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
                 </svg>
-                Received Applications
+                Applications
             </a>
             
             <a href="{{ route('client.active-contracts') }}" class="user-menu-item {{ request()->routeIs('client.active-contracts') ? 'active' : '' }}">
@@ -388,11 +388,11 @@
             </a>
             @endif
             
-            <a href="#" class="user-menu-item">
+            <a href="/stats" class="user-menu-item {{ request()->is('stats') ? 'active' : '' }}">
                 <svg class="user-menu-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path>
                 </svg>
-                Stats and trends
+                Stats and Trends
             </a>
             
             <div class="theme-toggle-btn">
@@ -409,11 +409,11 @@
                 </label>
             </div>
             
-            <a href="{{ route('settings') }}" class="user-menu-item">
+            <a href="{{ route('settings') }}" class="user-menu-item {{ request()->routeIs('settings') ? 'active' : '' }}">
                 <svg class="user-menu-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path>
                 </svg>
-                Account settings
+                Account Settings
             </a>
             
             <form method="POST" action="{{ route('logout') }}" class="mt-2">
