254007228a97ed95d3a439cd9a93bbce00dcd68d
mutheejj <johnmuthee547@gmail.com>
2025-10-31 11:48:18 +0300

    about: add placeholder page using site layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'About Us | Ajira Global')
+
+@section('content')
+<div class="py-16 bg-gradient-to-b from-blue-50 dark:from-gray-800 to-white dark:to-gray-900">
+	<div class="container mx-auto px-6 text-center">
+		<h1 class="text-4xl md:text-5xl font-bold text-gray-900 dark:text-white mb-4">About Ajira</h1>
+		<p class="text-xl text-gray-600 dark:text-gray-300 mb-10 max-w-3xl mx-auto">Our mission is to connect great clients with great talent.</p>
+	</div>
+</div>
+
+<div class="py-16 bg-white dark:bg-gray-900">
+	<div class="container mx-auto px-6">
+		<p class="text-gray-700 dark:text-gray-300">Company overview coming soon.</p>
+	</div>
+</div>
+@endsection
+
+
